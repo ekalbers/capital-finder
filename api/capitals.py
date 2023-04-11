eb23fd54de6413697d99b684ea79541df0ec41b8
Ethan Albers <ekalbers22@gmail.com>
2023-04-11 00:48:28 -0400

    attempting capital and country query

--- api/capitals.py
+++ api/capitals.py
@@ -1,7 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
-import json
 
 # https://restcountries.com/v3.1/name/{name}
 
@@ -9,20 +8,32 @@ import json
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        # s = self.path
-        # url_components = parse.urlsplit(s)
-        # query_string_list = parse.parse_qsl(url_components.query)
-        # dic = dict(query_string_list)
-        # name = dic.get('name')
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        country = dic.get('country')
+        capital = dic.get('capital')
 
-        url = 'https://restcountries.com/v3.1/name/america'
-        r = requests.get(url)
-        data = r.json()
+        if country:
+            url = f'https://restcountries.com/v3.1/name/{country}'
+            r = requests.get(url)
+            data = r.json()
+            country = data[0]['name']['common']
+            capital = data[0]['capital'][0]
+            message = f'The capital of {country} is {capital}.'
+
+        if capital:
+            url = f'https://restcountries.com/v3.1/capital/{capital}'
+            r = requests.get(url)
+            data = r.json()
+            country = data[0]['name']['common']
+            capital = data[0]['capital'][0]
+            message = f'{capital} is the capital of {country}.'
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        message = f"The {data[0]['capital']}"
         self.wfile.write(message.encode())
         return
 
