066673d73db9ff9d0e76e1ff8d52a6f0e52c1df6
Ethan Albers <ekalbers22@gmail.com>
2023-04-10 23:29:06 -0400

    added capital.py file

---
+++ api/capitals.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+import json
+
+# https://restcountries.com/v3.1/name/{name}
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        name = dic.get('name')
+
+        url = 'https://restcountries.com/v3.1/name/'
+        r = requests.get(url + name)
+        data = r.json()
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        self.wfile.write(json.load(data).encode())
+
+
